40ce4aa17f12eedb9a100b695265dcf36b66346e
mjsimon91 <mjsimon91@gmail.com>
2018-09-09 22:14:15 -0400

    Added a button for adding a new list

--- client/src/components/SideNav/SideNav.js
+++ client/src/components/SideNav/SideNav.js
@@ -4,6 +4,7 @@ import ListItem from "../ListItem"
 import Avatar from "../Avatar"
 import calendarIcon from "../../icons/baseline-calendar_today-black-18/2x/baseline_calendar_today_black_18dp.png"
 import listIcon from "../../icons/listItem/2x/baseline_list_black_18dp.png"
+import addListIcon from "../../icons/addListIcon/2x/baseline_add_circle_outline_black_18dp.png"
 
 class SideNav extends Component {
 
@@ -24,6 +25,9 @@ class SideNav extends Component {
                 icon = {listIcon}
                 listNameText = "To Do"
                 />
+                <button id="addList">
+                    <img id="addListIcon" src={addListIcon} alt="Add List"/>
+                </button>
             </div>
         )
     }
